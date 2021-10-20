7135ee4b3ce8f1d992a9ef0780676d40d87e23f6
yaduv1990 <92821858+yaduv1990@users.noreply.github.com>
2021-10-20 19:19:11 +0530

    Update TestRunner.java

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -1,17 +1,22 @@
 package StepDefinitions;
 
 import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
+
+
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Features/Moisturizer.feature",
+@CucumberOptions(features = "src/test/resources/Features",
 glue={"StepDefinitions"},
 monochrome = true,
 plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
 		"junit:target/cucumber-reports/Cucumber.xml",
-		"html:target/cucumber-reports/htmlreport"},
+"html:target/cucumber-reports/htmlreport"},
 tags =  "@functional")
 public class TestRunner {
+		
 
 }
